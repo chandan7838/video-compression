fc1ddfc79a4b757769f991bf97a32ace41e3f2a7
Chandan Chopadkar <chandanchopadkar@Chandans-MacBook-Pro.local>
2019-08-28 14:41:19 +0530

    constants added

--- android/src/main/java/com/reactlibrary/RNVideoCompressionModule.java
+++ android/src/main/java/com/reactlibrary/RNVideoCompressionModule.java
@@ -10,7 +10,8 @@ import com.facebook.react.bridge.Callback;
 public class RNVideoCompressionModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
-
+  private static final String DURATION_SHORT_KEY = "SHORT";
+  private static final String DURATION_LONG_KEY = "LONG";
   public RNVideoCompressionModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
@@ -19,6 +20,13 @@ public class RNVideoCompressionModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNVideoCompression";
+  }
+    @Override
+  public Map<String, Object> getConstants() {
+    final Map<String, Object> constants = new HashMap<>();
+    constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+    constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
+    return constants;
   }
   @ReactMethod
   public void show(String message, int duration) {
